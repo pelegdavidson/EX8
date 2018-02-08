650f1cdd75222f76730d842ec73c3d1e82f56066
pelegdavidson <pelegd13@gmail.com>
2018-02-08 23:38:47 +0200

    ex8

--- app/src/main/java/com/example/peleg/ex8/Main2Activity.java
+++ app/src/main/java/com/example/peleg/ex8/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.peleg.ex8;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+public class Main2Activity extends Activity implements View.OnClickListener{
     String s1;
     String s2;
     @Override
@@ -22,20 +23,22 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.btback).setOnClickListener(this);
         if(s1.length() > 0 &&
                 s2.length() > 0&&
-                (Integer.parseInt(s2)-32)*5/9==Integer.parseInt(s1)){
+                ((Integer.parseInt(s2)*(9/5)) + 32)==Integer.parseInt(s1)){
            TextView tv =  findViewById(R.id.tv2);
            tv.setText(res.getString(R.string.answer1 ,s2,s1));
         }else if(s1.length() > 0 &&
                 s2.length() > 0&&
-                (Integer.parseInt(s1)-32)*5/9!=Integer.parseInt(s2)){
+                ((Integer.parseInt(s2)*(9/5)) + 32)!=Integer.parseInt(s1)){
             TextView tv =  findViewById(R.id.tv2);
             tv.setText(res.getString(R.string.answer2));
         }else if(s1.length() > 0){
             TextView tv =  findViewById(R.id.tv2);
-            tv.setText(res.getString(R.string.answer3 , s1,(32+Integer.parseInt(s1)*5/9)));
-        }else if(s1.length() > 0){
+            s2 = Double.toString((Integer.parseInt(s1)-32.0)*(5.0/9.0));
+            tv.setText(res.getString(R.string.answer3 , s1,((Integer.parseInt(s1)-32.0)*(5.0/9.0))));
+        }else if(s2.length() > 0){
             TextView tv =  findViewById(R.id.tv2);
-                    tv.setText(res.getString(R.string.answer4 ,s2,(Integer.parseInt(s2)-32)*5/9));
+            s1 = Double.toString(Integer.parseInt(s2)*(9.0/5.0)) + 32.0;
+            tv.setText(res.getString(R.string.answer4 ,s2,(Integer.parseInt(s2)*(9.0/5.0)) + 32.0));
         }
 
     }
